3745919335032a473d6c616df5be4ab0b5ed3d3e
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-06-19 07:54:17 +0000

    initiated cordinates with axis

--- day1.js
+++ day1.js
@@ -2,7 +2,25 @@ const extractPath = () => {
   return Deno.readTextFileSync("input.txt").split(",");
 };
 
+const calcShortestPath = (cordinatesWithAxis, route) => {
+  return cordinatesWithAxis;
+};
+
 const shortestPath = () => {
   const path = extractPath();
-  return path;
+
+  if (path[0].startsWith("R")) {
+    return path.reduce(calcShortestPath, [0, 0, 0]);
+  }
+
+  return path.reduce(calcShortestPath(), [0, 0, 3]);
 };
+
+console.log(shortestPath());
+
+/**
+ * x - 0
+ * y - 1
+ * -x - 2
+ * -y - 3
+ */
