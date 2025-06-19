a0f0020d397a7b2b991149523318f1244cffa7b9
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-06-19 08:50:35 +0000

    added x axis increment steps

--- day1.js
+++ day1.js
@@ -2,8 +2,18 @@ const extractPath = () => {
   return Deno.readTextFileSync("input.txt").split(",");
 };
 
-const calcShortestPath = (cordinatesWithAxis, route) => {
-  return cordinatesWithAxis;
+const calcShortestPath = ([x, y, axis], route) => {
+  const steps = Number(route.split(/R|L/)[1]);
+
+  const rightAndYaxis = route.startsWith("R") && axis === 1;
+  const leftAndNegativeYaxis =
+    route.startsWith("L") && cordinatesWithAxis.at(2) === 1;
+
+  if (rightAndYaxis && leftAndNegativeYaxis) {
+    x += steps;
+    return [x, y, 0];
+  }
+  return [x, y, axis];
 };
 
 const shortestPath = () => {
@@ -13,7 +23,7 @@ const shortestPath = () => {
     return path.reduce(calcShortestPath, [0, 0, 0]);
   }
 
-  return path.reduce(calcShortestPath(), [0, 0, 3]);
+  return path.reduce(calcShortestPath, [0, 0, 3]);
 };
 
 console.log(shortestPath());
